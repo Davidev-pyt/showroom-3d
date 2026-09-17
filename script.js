3727cf0a292f06016c671013e7f1b80cfca88dd7
Davidev-pyt <serranojoseivan4@gmail.com>
2026-09-17 12:13:15 -0300

    fix: alinhada raiz do live server e corrigidos caminhos de mime type dos scripts

--- script.js
+++
@@ -1,1 +0,0 @@
-console.log("Motor M Power da BMW ativo!");
---
+++ script.js.js
@@ -0,0 +1,7 @@
+const palco_3d = document.getElementById('canvas-3d');
+
+function inicializarShowroom() {
+    console.log("Motores tridimensionais da BMW M Sport monitorados com sucesso...");
+}
+
+inicializarShowroom();
