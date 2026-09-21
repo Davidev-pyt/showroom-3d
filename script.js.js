e0e15773ea655f12f66f469b842928ee68dfa508
Davidev-pyt <serranojoseivan4@gmail.com>
2026-09-21 11:50:53 -0300

    feat: corrigido posicionamento dos botoes de cores e fluxo do script finalizado

--- script.js.js
+++ script.js.js
@@ -1,7 +1,27 @@
-const palco_3d = document.getElementById('canvas-3d');
+// 1. CAPTURA OS ELEMENTOS DA VITRINE
+const elemento_imagem = document.getElementById('bmw-imagem');
+const botao_azul = document.getElementById('btn-azul');
+const botao_preto = document.getElementById('btn-preto');
+const botao_branco = document.getElementById('btn-branco');
 
-function inicializarShowroom() {
-    console.log("Motores tridimensionais da BMW M Sport monitorados com sucesso...");
-}
+//ARQUIVOS LOCAIS OFFLINE!
+const foto_azul = "carro-azul.png";
+const foto_preta = "carro-preto.png";
+const foto_branca = "carro-branco.png";
 
-inicializarShowroom();
+// 2. EVENTOS DE CLIQUE PARA TROCA DE COR INSTANTÂNEA
+botao_azul.addEventListener('click', function() {
+    elemento_imagem.src = foto_azul;
+    console.log("Lataria atualizada: Azul M Sport.");
+});
+
+botao_preto.addEventListener('click', function() {
+    elemento_imagem.src = foto_preta;
+    console.log("Lataria atualizada: Preto Sapphire.");
+});
+
+// Certifique-se de que a função fecha corretamente com as chaves!
+botao_branco.addEventListener('click', function() {
+    elemento_imagem.src = foto_branca;
+    console.log("Lataria updated: Branco Alpine.");
+});
